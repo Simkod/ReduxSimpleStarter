d49144713d3c27389dc363ee501a79869591f0d5
daniel.simko <daniel.simko@betterask.erni>
2023-02-13 23:48:05 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './Components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBFt3nUIA_cT_Ya68hp7_EgVN3Z0bcHqaM';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create new component that produces some html
+const App = () => 
+{
+    return (
+        <div> 
+            <SearchBar /> 
+        </div>
+        );
+}
+
+// Display the component's generated HTMLs in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
