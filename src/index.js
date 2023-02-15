e41f6e7e90a73f912e9c5a5a0f963eccdee92add
daniel.simko <daniel.simko@betterask.erni>
2023-02-15 16:43:40 +0100

    youtube search data fetching

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './Components/search_bar';
 
 const API_KEY = 'AIzaSyBFt3nUIA_cT_Ya68hp7_EgVN3Z0bcHqaM';
+
+YTSearch(
+    {key: API_KEY, term: 'surfboards'},
+    function(data) {console.log(data);}
+    );
 
 // Create new component that produces some html
 const App = () => 
@@ -12,7 +18,7 @@ const App = () =>
         <div> 
             <SearchBar /> 
         </div>
-        );
+    );
 }
 
 // Display the component's generated HTMLs in the DOM
